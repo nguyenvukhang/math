8042d0fabf0c2b68d1dfc479c2a1bc70979bee00
nguyenvukhang <brew4k@gmail.com>
2023-10-24 23:16:15 -0400

    fix: update close-all to not close template

--- scripts/close-all.js
+++ scripts/close-all.js
@@ -1,9 +1,11 @@
 import { readFileSync, writeFileSync } from 'fs'
 import { getPosts } from './utils.js'
 
-getPosts().forEach((filepath) => {
-  const text = readFileSync(filepath, 'utf-8')
-  const lines = text.split('\n')
-  const output = lines.filter((v) => v.replace(/\s/g, '') !== 'open:true')
-  writeFileSync(filepath, output.join('\n'))
-})
+getPosts()
+  .filter((v) => !v.includes('template'))
+  .forEach((filepath) => {
+    const text = readFileSync(filepath, 'utf-8')
+    const lines = text.split('\n')
+    const output = lines.filter((v) => v.replace(/\s/g, '') !== 'open:true')
+    writeFileSync(filepath, output.join('\n'))
+  })
